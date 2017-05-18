389ff968dc08a491573fc0f391a017da562bb7a9
sakuma <soramamenatan@gmail.com>
2017-05-18 17:07:52 +0900

    ジョイパッド２

--- GameDeveloment/JoyPad.cpp
+++ GameDeveloment/JoyPad.cpp
@@ -24,11 +24,12 @@ struct SearchJoyPadSet
 	}
 };
 
-BOOL CALLBACK EnumObjectCallBack(LPCDIDEVICEOBJECTINSTANCE pInstance, void* pvRef)
-{
-	//次を検索
-	return DIENUM_CONTINUE;
-}
+////検出した入力オブジェクト1個ごとに呼ばれるコールバック
+//BOOL CALLBACK EnumObjectCallBack(LPCDIDEVICEOBJECTINSTANCE pInstance, void* pvRef)
+//{
+//	//次を検索
+//	return DIENUM_CONTINUE;
+//}
 
 //検出したジョイパッドごとに呼ばれるコールバック
 BOOL CALLBACK EnumJoyPadCallBack(LPCDIDEVICEINSTANCE lpddi, void* pvRef)
@@ -41,8 +42,18 @@ BOOL CALLBACK EnumJoyPadCallBack(LPCDIDEVICEINSTANCE lpddi, void* pvRef)
 	LPDIRECTINPUTDEVICE8 dev;
 	//コントローラごとにデバイスを生成
 	ret = param->dinput->CreateDevice(lpddi->guidInstance,&dev,NULL);
+	//失敗したら
 	if (ret != DI_OK)
 		goto NEXT;
+
+	//データ形式を設定(拡張機能つきジョイスティック)
+	ret = dev->SetDataFormat(&c_dfDIJoystick2);
+	//失敗したら
+	if (ret != DI_OK)
+		goto NEXT;
+
+	//配列に追加
+	param->joyPadDev.push_back(dev);
 NEXT:
 	//次を検索
 	return DIENUM_CONTINUE;
@@ -62,14 +73,21 @@ JoyPad::JoyPad()
 //----------------------------------------------------------------------
 JoyPad::~JoyPad()
 {
+	//配列にあるデバイスを開放
+	std::vector<JoyPadSet>::iterator it;
+	for (it = m_joyPadSet.begin(); it != m_joyPadSet.end(); it++)
+	{
+		//解放
+		SafeRelease(it->inputDevice);
+	}
 }
 
 //----------------------------------------------------------------------
-//! @brief 
+//! @brief ジョイパッドの初期設定
 //!
-//! @param[in] 
+//! @param[in] windouハンドル
 //!
-//! @return 
+//! @return 通るかどうか
 //----------------------------------------------------------------------
 bool JoyPad::Initialize(HWND window)
 {
@@ -85,6 +103,70 @@ bool JoyPad::Initialize(HWND window)
 	param.dinput = m_pDInput;
 	//利用可能なデバイスを列挙する
 	ret = m_pDInput->EnumDevices(DI8DEVCLASS_GAMECTRL, EnumJoyPadCallBack, &param, DIEDFL_ATTACHEDONLY);
-	
+	//失敗したら
+	if (ret != DI_OK)
+		return false;
+	//デバイス配列を設定
+	std::vector<LPDIRECTINPUTDEVICE8>::iterator it;
+	for (it = param.joyPadDev.begin(); it != param.joyPadDev.end(); it++)
+	{
+		LPDIRECTINPUTDEVICE8 dev = *it;
+		//アプリがアクティブ時にデバイスを排他アクセスに設定
+		ret = dev->SetCooperativeLevel(window, DISCL_EXCLUSIVE | DISCL_FOREGROUND);
+		//失敗したら
+		if (ret != DI_OK)
+			continue;
+		//アクセス権を要求
+		ret = dev->Acquire();
+		//失敗したら
+		if (ret != DI_OK)
+			continue;
+		//デバイス1個分の情報
+		JoyPadSet initPad;
+		initPad.inputDevice = dev;
+		//配列に追加する
+		m_joyPadSet.push_back(initPad);
+	}
 	return true;
+}
+
+//----------------------------------------------------------------------
+//! @brief 更新
+//!
+//! @param[in] なし
+//!
+//! @return なし
+//----------------------------------------------------------------------
+void JoyPad::Update()
+{
+	//すべてのデバイスについて処理する
+	std::vector<JoyPadSet>::iterator it;
+	for (it = m_joyPadSet.begin(); it != m_joyPadSet.end(); it++)
+	{
+		HRESULT ret;
+		//情報を更新する
+		it->inputDevice->Poll();
+		//失敗したら
+		if (ret != DI_OK)
+		{
+			//アクセス権を要求
+			ret = it->inputDevice->Acquire();
+			while (ret == DIERR_INPUTLOST)
+			{
+				//アクセス権を要求
+				ret = it->inputDevice->Acquire();
+			}
+		}
+		//前フレームの入力情報をバックアップ
+		it->joypadOld = it->joypad;
+		//入力情報を取得
+		ret = it->inputDevice->GetDeviceState(sizeof(DIJOYSTATE2), &it->joypad);
+		//失敗したら
+		if (ret != DI_OK)
+			continue;
+		if ((BYTE)(it->joypad.rgbButtons[0] & 0x80 == 0))
+		{
+			//ボタンが押されている
+		}
+	}
 }
--- GameDeveloment/JoyPad.h
+++ GameDeveloment/JoyPad.h
@@ -14,6 +14,17 @@
 #include <dinput.h>
 #include <vector>
 
+template<typename T>
+
+//解放
+void SafeRelease(T*& p)
+{
+	if (p)
+	{
+		p->Release();
+		p = NULL;
+	}
+}
 class JoyPad
 {
 public:
@@ -21,9 +32,28 @@ public:
 	JoyPad();
 	//デストラクタ
 	~JoyPad();
+	//ジョイパッドの初期設定
 	bool Initialize(HWND window);
+	//更新
+	void Update();
 private:
+	//ジョイパッド1個分の情報
+	struct JoyPadSet
+	{
+		JoyPadSet()
+		{
+			inputDevice = NULL;
+		}
+		//デバイス
+		LPDIRECTINPUTDEVICE8 inputDevice;
+		//入力情報
+		DIJOYSTATE2 joypad;
+		//前回の入力情報
+		DIJOYSTATE2 joypadOld;
+	};
 	//DirectInputインスタンス
 	LPDIRECTINPUT8 m_pDInput;
 
+	//ジョイパッド配列
+	std::vector<JoyPadSet>m_joyPadSet;
 };
